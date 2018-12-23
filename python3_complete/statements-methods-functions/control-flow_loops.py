4dad7afc18e6b13e6345d10c9c364a3b06dc377f
klcantrell <cantrellkalalau@gmail.com>
2018-12-22 22:48:38 -0500

    while loops

--- python3_complete/statements-methods-functions/control-flow_loops.py
+++ python3_complete/statements-methods-functions/control-flow_loops.py
@@ -17,6 +17,7 @@ else:
     print('I do not know much')
 
 # for loops
+
 my_numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for number in my_numbers_list:
     if number % 2 == 0:
@@ -32,3 +33,27 @@ for a, b in my_list:
 my_dict = {'k1': 1, 'k2': 2, 'k3': 3}
 for key, value in my_dict.items():
     print(f'value is {value} for key {key}')
+
+# while loops
+
+x = 0
+while x < 5:
+    print(f'The current value of x is {x}')
+    x += 1
+else:
+    print('x is not less than 5')
+
+array_2 = [1, 2, 3]
+for item in array_2:
+    pass
+print('end of loop through array_2')
+name = 'Sammy'
+for letter in name:
+    if letter == 'a':
+        continue
+    print(letter)
+for num in array_2:
+    if (num == 2):
+        print('breaking out of iterating through array_2')
+        break
+    print(num)
