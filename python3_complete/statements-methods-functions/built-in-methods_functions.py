ca7566fe22f4fc728d000f506b2f5f2df20e6ca1
klcantrell <cantrellkalalau@gmail.com>
2018-12-28 10:56:24 -0500

    map, filter, and lambdas

--- python3_complete/statements-methods-functions/built-in-methods_functions.py
+++ python3_complete/statements-methods-functions/built-in-methods_functions.py
@@ -89,3 +89,51 @@ def args_and_kwargs(*args, **kwargs):  # order matters and can't mix
 
 
 args_and_kwargs(10, 20, 30, fruit='orange', food='eggs')
+
+# map, filter, lambdas
+
+# # map
+
+
+def square(num):
+    return num**2
+
+
+my_nums = [1, 2, 3, 4, 5, 6]
+
+for item in map(square, my_nums):
+    print(item)
+
+print(list(map(square, my_nums)))
+
+
+def splicer(mystring):
+    if len(mystring) % 2 == 0:
+        return 'EVEN'
+    else:
+        return mystring[0]
+
+
+my_names = ['Kal', 'Andy', 'Nate', 'Rob']
+
+print(list(map(splicer, my_names)))
+
+# # filter
+
+
+def check_even(num):
+    return num % 2 == 0
+
+
+for n in filter(check_even, my_nums):
+    print(n)
+
+print(list(filter(check_even, my_nums)))
+
+# # lambda expression
+
+lambda num: num ** 2
+
+print(list(map(lambda num: num ** 2, my_nums)))
+
+print(list(map(lambda name: name[::-1], my_names)))
